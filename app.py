f1617c1218db3c6d5dc92d9fbc199810c5ea175b
Станислав-gif <ivanovstas102@gmail.com>
2024-11-25 18:34:45 +0500

    Добавлена модель для заявки данных,так же добавлено хранилище и метод POST для создания новой заявки

--- app.py
+++ app.py
@@ -1,9 +1,27 @@
-from fastapi import FastAPI
+import datetime
+from typing import Optional
+from fastapi import FastAPI, requests
 from fastapi.responses import HTMLResponse
- 
+from pydantic import BaseModel
+
+
+class Request(BaseModel):
+    id: int
+    date_added: datetime.datetime
+    device: str
+    problemtype: str
+    description: str
+    client: str
+    status: str #в ожидании,в работе,выполнено
+
+repo = []
+
 app = FastAPI()
- 
-@app.get("/")
-def read_root():
-    html_content = "<h2>Hello METANIT.COM!</h2>"
-    return HTMLResponse(content=html_content)
+
+message = ""
+
+@app.post("/requests/", response_model=Request)
+def get_requests(param: Optional[int] = None):
+    if param:
+        return {"repo": [o for o in repo if o.number == param]}
+    return {"repo": repo}
