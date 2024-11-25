05ae6339d66cff076dfa5238b9c11b6b4254c326
Станислав-gif <ivanovstas102@gmail.com>
2024-11-25 19:20:22 +0500

    Добавлен Get запрос для поиска заявки по параметру

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 import datetime
 from enum import Enum
 from typing import List, Optional
-from fastapi import FastAPI, requests
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI, Form, HTTPException
 from pydantic import BaseModel
 
 
@@ -27,12 +26,27 @@ repo = [
             problemtype="Не включается", 
             description="Не включается экран",
             client="Иван Иванов", 
-            status="в ожидании")
+            status="в ожидании"),
+    Request(id=2, 
+            startDate=datetime.date(2024, 11, 1), 
+            device="Телефон", 
+            problemtype="Не включается", 
+            description="Не включается экран",
+            client="Александр Иванов", 
+            status="в ожидании"),
 ]
-next_id = 2
+
+next_id = 3
+
 app = FastAPI()
 
 @app.get("/requests/", response_model=List[Request])
 def get_requests():
     return repo
+@app.get("/requests/{request_id}", response_model=Request)
+def get_request(request_id: int):
+    for db_request in repo:
+        if db_request.id == request_id:
+            return db_request
+    raise HTTPException(status_code=404, detail="Заявка не найдена")
 
