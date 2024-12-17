d8c1e7d179d6260935103e267bfe589f0fadd424
Станислав-gif <ivanovstas102@gmail.com>
2024-12-17 19:29:41 +0500

    Сделан пункт 2.2

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import datetime
 from enum import Enum
-from typing import List, Optional
+from typing import Annotated, List, Optional
 from fastapi import FastAPI, Form, HTTPException
 from pydantic import BaseModel
 
@@ -13,11 +13,16 @@ class Request(BaseModel):
     description: str
     client: str
     status: str
+    endDate: Optional[datetime.date] = None
+    assignee: Optional[str] = None  # Ответственный за выполнение
+    comments: Optional[List[str]] = []
 
-class RequestStatus(str, Enum):
-    pending = "в ожидании"
-    in_progress = "в работе"
-    completed = "выполнено"
+class UpdateRequestDTO(BaseModel):
+    number: int 
+    status: Optional[str] = ""
+    description: Optional[str] = ""
+    assignee: Optional[str] = ""
+    comments: Optional[str] = str
 
 repo = [
     Request(id=1, 
@@ -40,9 +45,17 @@ next_id = 3
 
 app = FastAPI()
 
-@app.get("/requests", response_model=List[Request])
-def get_requests():
-    return repo
+message = ""
+
+@app.get("/requests")
+def get_request(param = None):
+    global message
+    buffer = message
+    message = ""
+    if(param):
+        return { "repo": [o for o in repo if o.id == int(param)],"message": message}
+    return {"repo" : repo ,"message" : buffer}
+
 @app.get("/requests/{request_id}", response_model=Request)
 def get_request(request_id: int):
     for db_request in repo:
@@ -50,9 +63,8 @@ def get_request(request_id: int):
             return db_request
     raise HTTPException(status_code=404, detail="Заявка не найдена")
 
-@app.post("/requests")
+@app.post("/requests", response_model=Request)
 def create_request(
-    id: int = Form(...),
     startDate: str = Form(...), 
     device: str = Form(...),
     problemtype: str = Form(...),
@@ -60,9 +72,10 @@ def create_request(
     client: str = Form(...),
     status: str = Form(...)
 ):
+    global next_id
     new_request = Request(
-        id=id,
-        startDate=startDate,
+        id=next_id,
+        startDate=datetime.datetime.strptime(startDate, "%Y-%m-%d").date(),  
         device=device,
         problemtype=problemtype,
         description=description,
@@ -70,4 +83,25 @@ def create_request(
         status=status
     )
     repo.append(new_request)
+    next_id += 1 
     return new_request
+
+@app.post("/update")
+def update_request(dto: Annotated[UpdateRequestDTO , Form()]):
+    global message
+    for o in repo:
+        if o.id == dto.number:
+            if dto.status != o.status and dto.status != "":
+                o.status = dto.status
+                message += f"Статус заявки №${o.number} изменен\n"
+                if(o.status == "выполнено"):
+                    message += f"Заявки №{o.number} Завершено\n"
+                    o.endDate = datetime.datetime.now()
+            if dto.description != "":
+                o.description = dto.description
+            if dto.assignee != "":
+                o.assignee = dto.assignee
+            if dto.comments != None and dto.comments != "":
+                o.comments.append(dto.comments)
+            return o
+    return "Не найдено"
